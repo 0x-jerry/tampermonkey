99f45825ea89911f1980d6d5bef2e3cca9506de4
Jerry Wang <x.jerry.wang@gmail.com>
2023-08-28 10:10:19 +0800

    fix: zhihu links

--- un-redirect.user.js
+++ un-redirect.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         No Redirect
 // @namespace    http://tampermonkey.net/
-// @version      1.2.1
+// @version      1.2.2
 // @updateURL    https://github.com/0x-jerry/tampermonkey/raw/main/un-redirect.user.js
 // @downloadURL  https://github.com/0x-jerry/tampermonkey/raw/main/un-redirect.user.js
 // @description  Try to save the time, skip redirect in search result page, current only support google.
@@ -40,12 +40,13 @@ const configs = [
   },
 ]
 
-;(function () {
+;(async function () {
   'use strict'
 
   console.log('Un redirect loaded!')
 
   const handler = getHandler()
+  await sleep(100)
 
   handler?.()
 })()
@@ -62,9 +63,7 @@ function getHandler() {
 }
 
 async function handleCsdnLinks() {
-  await sleep(100)
-
-  doWithSelectorAll('article a', (el) => {
+  captureRedirectLinks('A', (el) => {
     const url = el.href
     if (!url) return
 
@@ -75,11 +74,12 @@ async function handleCsdnLinks() {
 
     const realUrl = u.toString()
 
-    overrideLinkClick(el, realUrl)
+    return realUrl
   })
 }
+
 function handleZhihuLinks() {
-  doWithSelectorAll('a', (el) => {
+  captureRedirectLinks('A', (el) => {
     const url = el.href
     if (!url) return
 
@@ -89,12 +89,12 @@ function handleZhihuLinks() {
 
     const realUrl = u.searchParams.get('target')
 
-    overrideLinkClick(el, realUrl)
+    return realUrl
   })
 }
 
 function handleBingSearchResult() {
-  doWithSelectorAll('#b_results a', (el) => {
+  captureRedirectLinks('A', (el) => {
     // https://www.bing.com/ck/a?!&&p=f3a402544cfb2e9aJmltdHM9MTY5MjgzNTIwMCZpZ3VpZD0wN2EwMTc0Yi0wMGRkLTYxYjctMzBhZC0wNDE5MDFiYjYwYjgmaW5zaWQ9NTU0OQ&ptn=3&hsh=3&fclid=07a0174b-00dd-61b7-30ad-041901bb60b8&psq=hello&u=a1aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g_dj1tSE9OTmNaYndEWQ&ntb=1
     const url = el.href
     if (!url) return
@@ -107,23 +107,26 @@ function handleBingSearchResult() {
       return
     }
 
-    const matcher = /var u = "[^"]+"/m
+    return {
+      matched: shouldSkip,
+      getUrl: async () => {
+        const matcher = /var u = "[^"]+"/m
 
-    overrideLinkClick(el, async () => {
-      const resp = await fetch(url)
+        const resp = await fetch(url)
 
-      // should match the next line url
-      // var u = "https://www.youtube.com/watch?v=mHONNcZbwDY";
-      const html = await resp.text()
-      const _matchedUrl = html.match(matcher).at(0)
+        // should match the next line url
+        // var u = "https://www.youtube.com/watch?v=mHONNcZbwDY";
+        const html = await resp.text()
+        const _matchedUrl = html.match(matcher).at(0)
 
-      return _matchedUrl?.slice('var u = "'.length, -1)
-    })
+        return _matchedUrl?.slice('var u = "'.length, -1)
+      },
+    }
   })
 }
 
 function handleGoogleSearchResult() {
-  doWithSelectorAll('a', (el) => {
+  captureRedirectLinks('A', (el) => {
     const url = el.href
 
     if (!url) return
@@ -132,7 +135,7 @@ function handleGoogleSearchResult() {
 
     const realUrl = url.startsWith('http') ? url : u.searchParams.get('url')
 
-    overrideLinkClick(el, realUrl)
+    return realUrl
   })
 }
 
@@ -140,39 +143,45 @@ function handleGoogleSearchResult() {
 
 /**
  *
- * @param {HTMLLinkElement} el
- * @param {string | (() => Promise<string> | string) | undefined} realUrl
+ * @param {string | ((el: HTMLElement) => boolean) } filter filter with tagname or use a custom function
+ * @param {(el: HTMLElement) => string | undefined | {matched: boolean, getUrl: () => Promise<string>}} getMatchResult
  */
-function overrideLinkClick(el, realUrl) {
-  if (!realUrl) return
+function captureRedirectLinks(filter, getMatchResult) {
+  const selectorFilter =
+    typeof filter === 'string' ? (el) => el.tagName === filter.toUpperCase() : filter
 
-  const isFn = typeof realUrl === 'function'
+  document.addEventListener('click', async (ev) => {
+    const links = ev.composedPath().filter(selectorFilter)
 
-  el.onclick = async (e) => {
-    e.preventDefault()
+    for (const link of links) {
+      try {
+        const result = getMatchResult(link)
 
-    const _realUrl = isFn ? await realUrl() : realUrl
+        if (typeof result === 'object') {
+          if (result?.matched) {
+            ev.preventDefault()
+            const realUrl = await result.getUrl()
+            if (!realUrl) continue
 
-    if (!_realUrl) return
+            console.debug(`matched redirect link`, result, link)
+            window.open(realUrl, '_blank')
+            return
+          }
 
-    window.open(_realUrl, '_blank')
-  }
+          continue
+        }
 
-  el.setAttribute('data-real-url', isFn ? 'fn' : realUrl)
-}
+        if (result) {
+          console.debug(`matched redirect link`, result, link)
 
-/**
- *
- * @param {string} selector
- * @param {(el: HTMLElement) => any} fn
- */
-function doWithSelectorAll(selector, fn) {
-  document.querySelectorAll(selector).forEach(async (el) => {
-    try {
-      await fn(el)
-    } catch (error) {
-      // ignore
-      console.debug('error', error)
+          ev.preventDefault()
+          window.open(result, '_blank')
+          return
+        }
+      } catch (error) {
+        // ignore
+        console.debug('error', error)
+      }
     }
   })
 }
