cdca6789d29e2e41166e078f587b8060ec9c27e9
Jerry Wang <x.jerry.wang@gmail.com>
2023-08-28 14:34:21 +0800

    refactor: add some common functions to utils.js

--- un-redirect.user.js
+++ un-redirect.user.js
@@ -14,178 +14,182 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bing.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zhihu.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=csdn.net
+// @require      ./utils.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
-/**
- * Define how to extract real url
- */
-const configs = [
-  {
-    matcher: /google\.com/,
-    handler: handleGoogleSearchResult,
-  },
-  {
-    matcher: /bing\.com/,
-    handler: handleBingSearchResult,
-  },
-  {
-    matcher: /zhihu\.com/,
-    handler: handleZhihuLogin,
-  },
-  {
-    matcher: /csdn\.net/,
-    handler: handleCsdnLinks,
-  },
-]
-
 ;(async function () {
   'use strict'
+  console.debug('Un redirect loaded!')
 
-  console.log('Un redirect loaded!')
+  $u.stringMatcher(location.href, [
+    {
+      test: /google\.com/,
+      handler: handleGoogleSearchResult,
+    },
+    {
+      test: /bing\.com/,
+      handler: handleBingSearchResult,
+    },
+    {
+      test: /zhihu\.com/,
+      handler: handleZhihuLogin,
+    },
+    {
+      test: /csdn\.net/,
+      handler: handleCsdnLinks,
+    },
+  ])
 
-  const handler = getHandler()
-  await sleep(100)
+  async function handleCsdnLinks() {
+    captureRedirectLinks(
+      'A',
+      /**
+       * @param {HTMLLinkElement} el
+       */
+      (el) => {
+        const url = el.href
+        if (!url) return
 
-  handler?.()
-})()
+        const u = new URL(url)
 
-function getHandler() {
-  const url = location.href
+        // skip csdn it self
+        if (u.host.endsWith('csdn.net')) return
 
-  for (const conf of configs) {
-    if (conf.matcher.test(url)) {
-      console.log('matched config', conf.matcher)
-      return conf.handler
-    }
-  }
-}
+        const realUrl = u.toString()
 
-async function handleCsdnLinks() {
-  captureRedirectLinks('A', (el) => {
-    const url = el.href
-    if (!url) return
-
-    const u = new URL(url)
-
-    // skip csdn it self
-    if (u.host.endsWith('csdn.net')) return
-
-    const realUrl = u.toString()
-
-    return realUrl
-  })
-}
-
-function handleZhihuLogin() {
-  captureRedirectLinks('A', (el) => {
-    const url = el.href
-    if (!url) return
-
-    const u = new URL(url)
-
-    if (u.host !== 'link.zhihu.com') return
-
-    const realUrl = u.searchParams.get('target')
-
-    return realUrl
-  })
-}
-
-function handleBingSearchResult() {
-  captureRedirectLinks('A', (el) => {
-    // https://www.bing.com/ck/a?!&&p=f3a402544cfb2e9aJmltdHM9MTY5MjgzNTIwMCZpZ3VpZD0wN2EwMTc0Yi0wMGRkLTYxYjctMzBhZC0wNDE5MDFiYjYwYjgmaW5zaWQ9NTU0OQ&ptn=3&hsh=3&fclid=07a0174b-00dd-61b7-30ad-041901bb60b8&psq=hello&u=a1aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g_dj1tSE9OTmNaYndEWQ&ntb=1
-    const url = el.href
-    if (!url) return
-
-    const u = new URL(url)
-
-    const shouldSkip = u.host.endsWith('bing.com') && u.pathname === '/ck/a'
-
-    if (!shouldSkip) {
-      return
-    }
-
-    return {
-      matched: shouldSkip,
-      getUrl: async () => {
-        const matcher = /var u = "[^"]+"/m
-
-        const resp = await fetch(url)
-
-        // should match the next line url
-        // var u = "https://www.youtube.com/watch?v=mHONNcZbwDY";
-        const html = await resp.text()
-        const _matchedUrl = html.match(matcher).at(0)
-
-        return _matchedUrl?.slice('var u = "'.length, -1)
+        return realUrl
       },
-    }
-  })
-}
+    )
+  }
 
-function handleGoogleSearchResult() {
-  captureRedirectLinks('A', (el) => {
-    const url = el.href
+  function handleZhihuLogin() {
+    captureRedirectLinks(
+      'A',
+      /**
+       * @param {HTMLLinkElement} el
+       */
+      (el) => {
+        const url = el.href
+        if (!url) return
 
-    if (!url) return
+        const u = new URL(url)
 
-    const u = new URL(url)
+        if (u.host !== 'link.zhihu.com') return
 
-    const realUrl = url.startsWith('http') ? url : u.searchParams.get('url')
+        const realUrl = u.searchParams.get('target')
 
-    return realUrl
-  })
-}
+        return realUrl
+      },
+    )
+  }
 
-// ---------- utils ---------
+  function handleBingSearchResult() {
+    captureRedirectLinks(
+      'A',
+      /**
+       * @param {HTMLLinkElement} el
+       */
+      (el) => {
+        // https://www.bing.com/ck/a?!&&p=f3a402544cfb2e9aJmltdHM9MTY5MjgzNTIwMCZpZ3VpZD0wN2EwMTc0Yi0wMGRkLTYxYjctMzBhZC0wNDE5MDFiYjYwYjgmaW5zaWQ9NTU0OQ&ptn=3&hsh=3&fclid=07a0174b-00dd-61b7-30ad-041901bb60b8&psq=hello&u=a1aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g_dj1tSE9OTmNaYndEWQ&ntb=1
+        const url = el.href
+        if (!url) return
 
-/**
- *
- * @param {string | ((el: HTMLElement) => boolean) } filter filter with tagname or use a custom function
- * @param {(el: HTMLElement) => string | undefined | {matched: boolean, getUrl: () => Promise<string>}} getMatchedResult
- */
-function captureRedirectLinks(filter, getMatchedResult) {
-  const selectorFilter =
-    typeof filter === 'string' ? (el) => el.tagName === filter.toUpperCase() : filter
+        const u = new URL(url)
 
-  document.addEventListener('click', async (ev) => {
-    const links = ev.composedPath().filter(selectorFilter)
+        const shouldSkip = u.host.endsWith('bing.com') && u.pathname === '/ck/a'
 
-    for (const link of links) {
-      try {
-        const result = getMatchedResult(link)
-
-        if (typeof result === 'object') {
-          if (result?.matched) {
-            ev.preventDefault()
-            const realUrl = await result.getUrl()
-            if (!realUrl) continue
-
-            console.debug(`matched redirect link`, result, link)
-            window.open(realUrl, '_blank')
-            return
-          }
-
-          continue
-        }
-
-        if (result) {
-          console.debug(`matched redirect link`, result, link)
-
-          ev.preventDefault()
-          window.open(result, '_blank')
+        if (!shouldSkip) {
           return
         }
-      } catch (error) {
-        // ignore
-        console.debug('error', error)
-      }
-    }
-  })
-}
 
-function sleep(ts = 100) {
-  return new Promise((resolve) => setTimeout(resolve, ts))
-}
+        return {
+          matched: shouldSkip,
+          getUrl: async () => {
+            const matcher = /var u = "[^"]+"/m
+
+            const resp = await fetch(url)
+
+            // should match the next line url
+            // var u = "https://www.youtube.com/watch?v=mHONNcZbwDY";
+            const html = await resp.text()
+            const _matchedUrl = html.match(matcher).at(0)
+
+            return _matchedUrl?.slice('var u = "'.length, -1)
+          },
+        }
+      },
+    )
+  }
+
+  function handleGoogleSearchResult() {
+    captureRedirectLinks(
+      'A',
+      /**
+       * @param {HTMLLinkElement} el
+       */
+      (el) => {
+        const url = el.href
+
+        if (!url) return
+
+        const u = new URL(url)
+
+        const realUrl = url.startsWith('http') ? url : u.searchParams.get('url')
+
+        return realUrl
+      },
+    )
+  }
+
+  // ---------- utils ---------
+
+  /**
+   *
+   * @param {string | ((el: HTMLElement) => boolean) } filter filter with tagname or use a custom function
+   * @param {(el: HTMLElement) => string | undefined | {matched: boolean, getUrl: () => Promise<string>}} getMatchedResult
+   */
+  function captureRedirectLinks(filter, getMatchedResult) {
+    const selectorFilter =
+      typeof filter === 'string' ? (el) => el.tagName === filter.toUpperCase() : filter
+
+    document.addEventListener('click', async (ev) => {
+      /**
+       * @type {HTMLElement[]}
+       */
+      // @ts-ignore
+      const links = ev.composedPath().filter(selectorFilter)
+
+      for (const link of links) {
+        try {
+          const result = getMatchedResult(link)
+
+          if (typeof result === 'object') {
+            if (result?.matched) {
+              ev.preventDefault()
+              const realUrl = await result.getUrl()
+              if (!realUrl) continue
+
+              console.debug(`matched redirect link`, result, link)
+              window.open(realUrl, '_blank')
+              return
+            }
+
+            continue
+          }
+
+          if (result) {
+            console.debug(`matched redirect link`, result, link)
+
+            ev.preventDefault()
+            window.open(result, '_blank')
+            return
+          }
+        } catch (error) {
+          console.debug('error', error)
+        }
+      }
+    })
+  }
+})()
---
+++ utils.js
@@ -0,0 +1,51 @@
+console.debug('Utils loaded!')
+
+globalThis.$u = {
+  sleep(ts = 100) {
+    return new Promise((resolve) => setTimeout(resolve, ts))
+  },
+  /**
+   *
+   * @param {string} selector
+   * @returns {Promise<HTMLElement>}
+   */
+  async waitElement(selector) {
+    return $u.when(() => document.querySelector(selector))
+  },
+
+  /**
+   *
+   * @template T
+   * @param {() => T } checker
+   * @param {number} timeout  default is 10 * 1000 (10s).
+   * @returns {Promise<T>}
+   */
+  async when(checker, timeout = 10 * 1000) {
+    const start = Date.now()
+
+    while (Date.now() - start < timeout) {
+      const pass = await checker()
+
+      if (pass) return pass
+
+      await $u.sleep(100)
+    }
+
+    throw new Error('Timeout')
+  },
+
+  /**
+   *
+   * @typedef MatcherConfig
+   * @prop {RegExp} test
+   * @prop {() => any} handler
+   *
+   * @param {MatcherConfig[]} configs
+   * @param {string} str
+   */
+  stringMatcher(str, configs) {
+    const hit = configs.find((n) => n.test.test(str))
+
+    return hit?.handler()
+  },
+}
