49934833399a378d42fd239c50b0e592d626d2f4
Jerry <x.jerry.wang@gmail.com>
2024-08-12 09:55:13 +0000

    perf: toggle theme, enable at started

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 - Quick Nav: Add convenient buttons to switch chapters [install](https://github.com/0x-jerry/tampermonkey/raw/main/out/happymh.user.js)
 - Refine Reading: Refine style of hetushu.com [install](https://github.com/0x-jerry/tampermonkey/raw/main/out/refine-reading.user.js)
 - Simple MMO: try to save the simple mmo world! [install](https://github.com/0x-jerry/tampermonkey/raw/main/out/smmo.user.js)
-- Toggle Theme: Very lightweight solution to toggle dark/light theme. Open devtools, find `button[data-toggle-theme=1]`, then trigger it's click event. [install](https://github.com/0x-jerry/tampermonkey/raw/main/out/toggle-theme.user.js)
+- Toggle Theme: Very lightweight solution to toggle dark/light theme. [install](https://github.com/0x-jerry/tampermonkey/raw/main/out/toggle-theme.user.js)
 - Un Login: Auto close login dialog at some site, support zhihu. [install](https://github.com/0x-jerry/tampermonkey/raw/main/out/un-login.user.js)
 - Un Redirect: Skip redirect at some search result page, support google/bing/zhihu/csdn/sspai. [install](https://github.com/0x-jerry/tampermonkey/raw/main/out/un-redirect.user.js)
 - Save the world: try to save the world! [install](https://github.com/0x-jerry/tampermonkey/raw/main/out/yunpan1.user.js)
--- src/toggle-theme.user.ts
+++ src/toggle-theme.user.ts
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Toggle Theme
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @updateURL    https://github.com/0x-jerry/tampermonkey/raw/main/out/toggle-theme.user.js
 // @downloadURL  https://github.com/0x-jerry/tampermonkey/raw/main/out/toggle-theme.user.js
-// @description  Very lightweight solution to toggle dark/light theme. Open devtools, find `button[data-toggle-theme=1]`, then trigger it's click event.
+// @description  Very lightweight solution to toggle dark/light theme.
 // @author       x.jerry.wang@gmail.com
 // @match        https://*/**
 // @match        http://*/**
@@ -16,30 +16,9 @@
 $u.run(async () => {
   'use strict'
 
-  const toggleButton = document.createElement('button')
-  toggleButton.style.display = 'none'
-  toggleButton.setAttribute('data-toggle-theme', '1')
-
-  document.body.append(toggleButton)
-
-  const enabledKey = '__0x_jerry_tampermonkey_toggle_theme__'
-
-  toggleButton.onclick = () => {
-    const value = localStorage.getItem(enabledKey)
-    const toggleValue = value === '1' ? '0' : '1'
-    localStorage.setItem(enabledKey, toggleValue)
-
-    doEffect()
-  }
-
   doEffect()
 
   function doEffect() {
-    const value = localStorage.getItem(enabledKey)
-    if (value !== '1') {
-      return
-    }
-
     // @ts-ignore
     GM_addStyle(`
       html {
