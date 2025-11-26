51f464ae7bcf81558126fda9b269f5fdff95480f
Jerry <x.jerry.wang@gmail.com>
2025-11-27 00:07:59 +0800

    chore: update snippets

--- .snippets/userscript.js
+++ .snippets/userscript.js
@@ -1,18 +1,11 @@
-const initScript = `// ==UserScript==
-// @name         Save the world
-// @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @updateURL    https://github.com/0x-jerry/tampermonkey/raw/main/out/x.user.js
-// @downloadURL  https://github.com/0x-jerry/tampermonkey/raw/main/out/x.user.js
-// @description  try to save the world!
-// @author       x.jerry.wang@gmail.com
-// @match        https://*.google.com/**
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
-// @run-at       document-end
-// @grant        none
-// ==/UserScript==
+const initScript = `import { defineHeader, run } from './utils'
 
-import { run } from './utils'
+export const config = defineHeader({
+  name: 'Script',
+  version: '1.0.0',
+  description: 'Try to save the world',
+  matches: ['https://*/*'],
+})
 
 run(async () => {
   // Your code here...
