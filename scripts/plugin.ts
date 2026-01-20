1f8279f7f0efe3e3d5aa75709390d85e4351683a
Jerry <x.jerry.wang@gmail.com>
2026-01-20 02:59:27 +0000

    refactor: clear definHeader body when build

--- scripts/plugin.ts
+++ scripts/plugin.ts
@@ -1,21 +1,24 @@
-import type { ExpressionStatement, Program } from '@oxc-project/types'
+import path from 'node:path'
+import type { Program } from '@oxc-project/types'
 import * as esrap from 'esrap'
 import ts from 'esrap/languages/ts'
+import { Visitor } from 'oxc-parser'
 import type { Plugin } from 'rolldown'
 import { DEFINE_HEADER_FN_NAME } from './utils'
 
 export function tampermonkey(): Plugin<void> {
-  return {
-    name: 'test-plugin',
-    async resolveId(source, importer, extraOption) {
-      const id = await this.resolve(source, importer, extraOption)
+  const configFile = path.resolve('src/utils/config.ts')
 
-      return id
-    },
+  return {
+    name: 'tampermonkey-plugin',
     transform(code, id, meta) {
+      if (id !== configFile) {
+        return
+      }
+
       if (!meta.ast) return
 
-      if (!removeDefineFunction(meta.ast)) {
+      if (!clearDefineFunctionBody(meta.ast, DEFINE_HEADER_FN_NAME)) {
         return
       }
 
@@ -29,37 +32,20 @@ export function tampermonkey(): Plugin<void> {
   }
 }
 
-function removeDefineFunction(ast: Program) {
-  const idx = ast.body.findIndex((node) => {
-    if (node.type === 'ExpressionStatement') {
-      return isCallable(node, DEFINE_HEADER_FN_NAME)
-    }
+function clearDefineFunctionBody(ast: Program, fnName: string): boolean {
+  let removed = false
 
-    return false
+  const visitor = new Visitor({
+    FunctionDeclaration(node) {
+      if (node.id?.name === fnName) {
+        if (node.body?.body.length) {
+          node.body.body = []
+          removed = true
+        }
+      }
+    },
   })
+  visitor.visit(ast)
 
-  if (idx >= 0) {
-    ast.body.splice(idx, 1)
-    return true
-  }
-
-  return false
-}
-
-function isCallable(node: ExpressionStatement, defineName: string) {
-  if (node.expression.type !== 'CallExpression') {
-    return
-  }
-
-  const callDecl = node.expression
-
-  if (callDecl.callee.type !== 'Identifier') {
-    return
-  }
-
-  if (callDecl.callee.name !== defineName) {
-    return
-  }
-
-  return true
+  return removed
 }
