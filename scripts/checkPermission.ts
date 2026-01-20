92d80abe501523dfec1ed1341c2de0609462c888
Jerry <x.jerry.wang@gmail.com>
2026-01-20 03:49:42 +0000

    chore: split scan permission utils

--- scripts/checkPermission.ts
+++ scripts/checkPermission.ts
@@ -5,18 +5,22 @@ const JS_LINE_COMMENT_RE = /\/\/[^\n]*/g
 const JS_MULTI_LINE_COMMENT_RE = /\/\*[\s\S]*?\*\//g
 const GM_PERMISSION_RE = /\bGM_[a-zA-Z0-9_]+\b/g
 
-export async function checkPermission(
-  config: ITamperMonkeyHeader,
-  outputFile: string,
-): Promise<string[]> {
-  const code = await readFile(outputFile, 'utf-8')
+export async function scanPermissions(file: string) {
+  const code = await readFile(file, 'utf-8')
 
   const permissions = code
     .replace(JS_LINE_COMMENT_RE, '')
     .replace(JS_MULTI_LINE_COMMENT_RE, '')
     .match(GM_PERMISSION_RE)
 
-  const required = [...new Set(permissions)]
+  return [...new Set(permissions)]
+}
+
+export async function checkPermission(
+  config: ITamperMonkeyHeader,
+  outputFile: string,
+): Promise<string[]> {
+  const required = await scanPermissions(outputFile)
   const granted = [...new Set(config.grants)] as string[]
 
   return required.filter((perm) => !granted.includes(perm))
