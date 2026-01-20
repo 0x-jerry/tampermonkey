0dca5f3e9de689946bd915d9302c5f9433e51fe1
Jerry <x.jerry.wang@gmail.com>
2026-01-20 06:40:18 +0000

    chore: applyStyle utils

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -4,3 +4,17 @@ export function css(
 ) {
   return String.raw(template, ...substitutions)
 }
+
+interface StyleVariableDeclaration {
+  [key: `--${string}`]: string | number | null
+}
+
+export function applyStyle(
+  el: HTMLElement,
+  style: Partial<CSSStyleDeclaration & StyleVariableDeclaration>,
+) {
+  for (const key in style) {
+    const val = Reflect.get(style, key)
+    el.style.setProperty(key, val)
+  }
+}
