ab0912e5adf66a6f3308e1ef7f20e3db1cb2a529
Jerry <x.jerry.wang@gmail.com>
2026-01-20 03:24:08 +0000

    fix: ensure multi-line comments are removed in checkPermission

--- scripts/checkPermission.ts
+++ scripts/checkPermission.ts
@@ -2,7 +2,8 @@ import { readFile } from 'node:fs/promises'
 import type { ITamperMonkeyHeader } from '../types'
 
 const JS_LINE_COMMENT_RE = /\/\/[^\n]*/g
-const GM_RE = /\bGM_[a-zA-Z0-9_]+\b/g
+const JS_MULTI_LINE_COMMENT_RE = /\/\*[\s\S]*?\*\//g
+const GM_PERMISSION_RE = /\bGM_[a-zA-Z0-9_]+\b/g
 
 export async function checkPermission(
   config: ITamperMonkeyHeader,
@@ -10,7 +11,10 @@ export async function checkPermission(
 ): Promise<string[]> {
   const code = await readFile(outputFile, 'utf-8')
 
-  const permissions = code.replace(JS_LINE_COMMENT_RE, '').match(GM_RE)
+  const permissions = code
+    .replace(JS_LINE_COMMENT_RE, '')
+    .replace(JS_MULTI_LINE_COMMENT_RE, '')
+    .match(GM_PERMISSION_RE)
 
   const required = [...new Set(permissions)]
   const granted = [...new Set(config.grants)] as string[]
