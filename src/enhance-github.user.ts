af827459fcfec983e18f51f1fb6319a906c7a4cd
Jerry <x.jerry.wang@gmail.com>
2026-08-06 23:45:03 +0800

    chore: release with enhance-github

--- src/enhance-github.user.ts
+++ src/enhance-github.user.ts
@@ -4,7 +4,7 @@ import dayjs from 'dayjs'
 
 defineHeader({
   name: 'Enhance GitHub',
-  version: '1.0.7',
+  version: '1.0.8',
   description: 'Enhance GitHub with useful features like displaying repository size',
   matches: ['https://github.com/*/*'],
   runAt: 'document-idle',
